123743c7e74ded8e26bd4f175527582e5055208e
Jonathan Marler <johnnymarler@gmail.com>
2022-03-09 00:31:17 -0700

    windows cmd-line args

--- inc/libc/private/fixedwidthints.h
+++ inc/libc/private/fixedwidthints.h
@@ -1,6 +1,10 @@
 #ifndef _FIXED_WIDTH_INTS_H
 #define _FIXED_WIDTH_INTS_H
 
-typedef long int64_t;
+#ifdef _WIN32
+    typedef long long int64_t;
+#else
+    typedef long int64_t;
+#endif
 
 #endif /* _FIXED_WIDTH_INTS_H */
--- src/zigstart.zig
+++ src/zigstart.zig
@@ -5,16 +5,13 @@ const c = struct {
     extern fn main(argc: c_int, argv: [*:null]?[*:0]u8) callconv(.C) c_int;
 };
 
-// TODO: remove this when command-line args work on Windows
-var windows_args = if (builtin.os.tag == .windows) [_:null]?[*:0]u8 { } else @compileError("only use this on Windows");
-
 pub fn main() u8 {
     var argc: c_int = undefined;
     const args: [*:null]?[*:0]u8 = blk: {
         if (builtin.os.tag == .windows) {
-            std.log.warn("command-line args not implemented on Windows!", .{});
-            argc = 0;
-            break :blk &windows_args;
+            const args = windowsArgsAlloc();
+            argc = @intCast(c_int, args.len);
+            break :blk args.ptr;
         }
         argc = @intCast(c_int, std.os.argv.len);
         break :blk @ptrCast([*:null]?[*:0]u8, std.os.argv.ptr);
@@ -25,4 +22,26 @@ pub fn main() u8 {
         while ((result & 0xff == 0)) result = result >> 8;
     }
     return @intCast(u8, result & 0xff);
+}
+
+// TODO: I'm pretty sure this could be more memory efficient
+fn windowsArgsAlloc() [:null]?[*:0]u8 {
+    const out_of_memory_msg = "Out Of Memory while decoding command line";
+
+    var argv_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    var tmp_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer tmp_arena.deinit();
+
+    var argv = std.ArrayListUnmanaged(?[*:0]u8) { };
+    var it = std.process.argsWithAllocator(tmp_arena.allocator()) catch |err| switch (err) {
+        error.OutOfMemory => @panic(out_of_memory_msg),
+        // TODO: would be nice to get the actual utf16 decode error name
+        error.InvalidCmdLine => @panic("Failed to decode command line"),
+    };
+    defer it.deinit();
+    while (it.next()) |tmp_arg| {
+        const arg = argv_arena.allocator().dupeZ(u8, tmp_arg) catch @panic(out_of_memory_msg);
+        argv.append(argv_arena.allocator(), arg) catch @panic(out_of_memory_msg);
+    }
+    return argv.toOwnedSliceSentinel(argv_arena.allocator(), null) catch @panic(out_of_memory_msg);
 }
