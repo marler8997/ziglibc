d90f40c2c7b3751f36bd7f70f2a0ea8570bfa3f1
Jonathan Marler <johnnymarler@gmail.com>
2022-10-23 21:38:44 -0600

    zig update

--- src/trace.zig
+++ src/trace.zig
@@ -2,8 +2,6 @@ const std = @import("std");
 const trace_options = @import("trace_options");
 
 pub fn log(comptime fmt: []const u8, args: anytype) void {
-    _ = fmt;
-    _ = args;
     if (trace_options.enabled) {
         std.log.scoped(.trace).info(fmt, args);
     }
