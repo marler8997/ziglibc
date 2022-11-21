d0374457ece5f718df5ad01ddb968e7f2df455ad
Felix "xq" Queißner <xq@random-projects.net>
2022-11-21 10:00:20 +0100

    Fixes size_t.h

--- inc/libc/private/size_t.h
+++ inc/libc/private/size_t.h
@@ -1,11 +1,6 @@
 #ifndef _PRIVATE_SIZET_H
 #define _PRIVATE_SIZET_H
 
-// TODO: fixme
-#ifdef _WIN32
-    typedef unsigned long long size_t;
-#else
-    typedef unsigned long size_t;
-#endif
+typedef __SIZE_TYPE__ size_t;
 
 #endif /* _PRIVATE_SIZET_H */
